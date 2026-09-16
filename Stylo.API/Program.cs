5f1c558ad75858431a38bcbdf916dfe21dce4f22
Mostafa Mahmoud <you@example.com>
2026-09-17 01:18:04 +0300

    Enable Swagger in production

--- Stylo.API/Program.cs
+++ Stylo.API/Program.cs
@@ -20,14 +20,13 @@ var app = builder.Build();
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stylo API v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stylo API v1");
+});
+
 
 app.UseHttpsRedirection();
 
